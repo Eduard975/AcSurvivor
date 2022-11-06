e800defdff92255882d8a0b52a4e230d63be3609
Eduard975 <theblackasasin@yahoo.com>
2022-11-06 13:06:21 +0200

    Avem movement cat de cat

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,53 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+int main() {
+  auto window = sf::RenderWindow{sf::VideoMode(728, 455), "Misto"};
+  window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
+  sf::Sprite player, background;
+  sf::Texture player_texture, background_texture;
+
+  player_texture.loadFromFile("../textures/player.png");
+  player.setTexture(player_texture);
+
+  background_texture.loadFromFile("../textures/background.jpg");
+  background.setTexture(background_texture);
+
+  player.setPosition(300, 400);
+
+  while (window.isOpen()) {
+    for (auto event = sf::Event{}; window.pollEvent(event);) {
+      switch (event.type) {
+      case sf::Event::Closed:
+        window.close();
+      case sf::Event::KeyPressed:
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+          // W key is pressed: move our character
+          player.move(0, -10);
         }
 
-        window.clear();
-        window.display();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+          // A key is pressed: move our character
+          player.move(-10, 0);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+          // S key is pressed: move our character
+          player.move(0, 10);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+          // D key is pressed: move our character
+          player.move(10, 0);
+        }
+      }
     }
+
+    window.clear();
+
+    window.draw(background);
+    window.draw(player);
+
+    window.display();
+  }
 }
